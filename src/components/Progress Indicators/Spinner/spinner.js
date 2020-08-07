9370b4739f025c9b15b2d9a50f35e37975afad81
Joseph Hart <josephrhart@gmail.com>
2020-08-07 11:43:28 -0500

    fix(lint): fixing lint errors

--- src/components/Progress Indicators/Spinner/spinner.js
+++ src/components/Progress Indicators/Spinner/spinner.js
@@ -1,52 +1,63 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './spinner.scss'
+import './spinner.scss';
 
-export const Spinner = (props) => {
-  
-  const { size } = props;
-  let calcSize = '50px';
-  let calcBorder = '1px';
+const Spinner = ({
+  background,
+  color,
+  inline,
+  label,
+  size,
+  children,
+}) => {
+  let calcSize;
+  let calcBorder;
 
   if (size) {
-
-    switch(size.toLowerCase()) {
+    switch (size.toLowerCase()) {
       case 'sm':
       case 'small':
-          calcSize = '16px';
-          calcBorder = '2px';
-          break;
-        case 'med':
-        case 'medium':
-          calcSize = '48px';
-          calcBorder = '5px';
-          break;
-        case 'lg':
-        case 'large':
-          calcSize = '80px';
-          calcBorder = '8px';
-          break;
+        calcSize = '16px';
+        calcBorder = '2px';
+        break;
+      case 'med':
+      case 'medium':
+        calcSize = '48px';
+        calcBorder = '5px';
+        break;
+      case 'lg':
+      case 'large':
+        calcSize = '80px';
+        calcBorder = '8px';
+        break;
+      default:
+        calcSize = '50px';
+        calcBorder = '1px';
+        break;
     }
   }
 
   return (
-    <div style={{ display: 'flex', flexDirection: props.inline ? 'row' : 'column', alignItems: 'center' }}>
+    <div style={{ display: 'flex', flexDirection: inline ? 'row' : 'column', alignItems: 'center' }}>
       <div style={{ position: 'relative', width: calcSize, height: calcSize }}>
         <div className="ab-spinner">
-          <div className="ab-spinner-icon" style={{ 
-            borderColor: props.background,
-            borderTopColor: props.color,
-            borderWidth: calcBorder
-          }} />
+          <div
+            className="ab-spinner-icon"
+            style={{
+              borderColor: background,
+              borderTopColor: color,
+              borderWidth: calcBorder,
+            }}
+          />
           <div className="ab-spinner-animation" />
-          {props.children}
+          {children}
         </div>
       </div>
-      { props.label &&
-        <span className="ab-spinner-label">{props.label}</span>
-      }
+      {label
+        && <span className="ab-spinner-label">{label}</span>}
     </div>
-  )};
+  );
+};
 
 Spinner.propTypes = {
   /** The color displayed within the Spinners channel. */
@@ -57,8 +68,12 @@ Spinner.propTypes = {
   inline: PropTypes.bool,
   /** Text that will be rendered with the Spinner. */
   label: PropTypes.string,
-  /** Specifies the size of the Spinner. Acceptable values are 'sm', 'small', 'med', 'medium', 'lg' and 'large'. */
+  /**
+   * Specifies the size of the Spinner.
+   * Acceptable values are 'sm', 'small', 'med', 'medium', 'lg' and 'large'.
+   * */
   size: PropTypes.string,
+  children: PropTypes.node,
 };
 
 Spinner.defaultProps = {
@@ -67,4 +82,7 @@ Spinner.defaultProps = {
   inline: false,
   label: '',
   size: 'small',
+  children: null,
 };
+
+export default Spinner;
